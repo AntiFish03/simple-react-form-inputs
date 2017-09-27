d56b451287b90787fc08f2f3f208d8248f199e18
Joseph Cota <jcota@getaroom.com>
2017-09-27 14:30:50 -0500

    Updating to use `prop-types` over `React.PropTypes`

--- src/auto-complete.jsx
+++ src/auto-complete.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import SimpleInput from './simple-input';
 import _ from 'lodash';
 import classnames from 'classnames';
